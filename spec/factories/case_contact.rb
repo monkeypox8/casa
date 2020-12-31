7ce5959b7667328987ef6a916bd99cf2c9a2440c
CW <compiledwrong+github@gmail.com>
2020-12-30 18:58:26 -0800

    Use long notes in report

--- spec/factories/case_contact.rb
+++ spec/factories/case_contact.rb
@@ -9,6 +9,15 @@ FactoryBot.define do
     contact_made { false }
     medium_type { CaseContact::CONTACT_MEDIUMS.first }
     want_driving_reimbursement { false }
+    note { "short note" }
+
+    trait :multi_line_note do
+      note { "line1\nline2\nline3" }
+    end
+
+    trait :long_note do
+      note { "1234567890" * 11 } # longer than NOTES_CHARACTER_LIMIT
+    end
 
     trait :miles_driven_no_reimbursement do
       miles_driven { 20 }
